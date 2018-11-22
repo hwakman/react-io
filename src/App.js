fe301db06c46e595ab03079e340f90c46f3b8b61
TOR <chayoud.p@arms-thai.com>
2018-11-22 17:08:29 +0700

    update

--- src/App.js
+++ src/App.js
@@ -3,39 +3,64 @@ import { Typography, Toolbar, Button } from '@material-ui/core';
 import './App.css';
 
 class TopMenu extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      locate: 'Home'
+    }
+  }
+  componentDidMount(){
+    this.changeLocation = this.changeLocation.bind(this);
+  }
+  changeLocation = val => {
+    this.setState({locate:val});
+  }
+  selection = () => {
+    switch(this.state.locate){
+      case 'Home':
+      return <Home />
+      break;
+      case 'Experience':
+      return <Experience />
+      break;
+      case 'Skills':
+      return <Skills />
+      break;
+      case 'Contact':
+      return <Contact />
+      break;
+    }
+  }
   render = () => {
     return (
-      <Toolbar variant="dense">
-        <Typography style={{flex:1}}>
-          <Button>Home</Button>
-        </Typography>
-        <Button>experience</Button>
-        <Button>skills set</Button>
-        <Button>contact</Button>
-      </Toolbar>
+      <Fragment>
+        <Toolbar variant="dense">
+          <Typography style={{flex:1}}>
+            <Button variant="outlined" onClick={this.changeLocation.bind(this,"Home")}>Home</Button>
+          </Typography>
+          <Button onClick={this.changeLocation.bind(this,"Experience")}>experience</Button>
+          <Button onClick={this.changeLocation.bind(this,"Skills")}>slills set</Button>
+          <Button onClick={this.changeLocation.bind(this,"Contact")}>contact</Button>
+        </Toolbar>
+        
+        <div className="content">
+          {this.selection()}
+        </div>
+      </Fragment>
     );
   }
 }
 
-class Content extends Component {
-  state = {
-    location: ''
-  }
-  render = () => {
-    return (
-      <div className="content">
-        <p>{this.state.location}</p>
-      </div>
-    )
-  }
-}
+const Home = () => <h1>Home</h1>;
+const Experience = () => <h1>Experience</h1>;
+const Skills = () => <h1>Skills</h1>;
+const Contact = () => <h1>Contact</h1>;
 
 class App extends Component {
   render() {
     return (
       <Fragment>
         <TopMenu />
-        <Content />
       </Fragment>
     );
   }
