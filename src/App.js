dd6b0f95c85e5bd9ce0811c0d9a7c0b24e7be833
TOR <chayoud.p@arms-thai.com>
2018-11-22 16:02:24 +0700

    update link

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
+import { Typography, Toolbar, Button } from '@material-ui/core';
+import { BrowserRouter as Link, Route } from 'react-router-dom';
 import './App.css';
+
+class TopMenu extends Component {
+  changeLink = locate => {
+    window.location.replace('/'+locate);
+  }
+  render = () => {
+    return (
+      <Toolbar variant="dense">
+        <Typography style={{flex:1}}>
+          <Button onClick={this.changeLink.bind(this,"")} variant="outlined">Home</Button>
+        </Typography>
+        <Button onClick={this.changeLink.bind(this,"experience")}>experience</Button>
+        <Button onClick={this.changeLink.bind(this,"skills")}>skills set</Button>
+        <Button onClick={this.changeLink.bind(this,"contact")}>contact</Button>
+      </Toolbar>
+    );
+  }
+}
+
+const Home = () => {
+  return (
+    <p>Home</p>
+  )
+}
+
+const Experience = () => {
+  return (
+    <p>Experience</p>
+  )
+}
+
+const Skills = () => {
+  return (
+    <p>Skills</p>
+  )
+}
+
+const Contact = () => {
+  return (
+    <p>Contact</p>
+  )
+}
+
+const Content = () => {
+  return (
+    <div className="content">
+      <Route exact path="/" component={Home} />
+      <Route path="/experience" component={Experience} />
+      <Route path="/skills" component={Skills} />
+      <Route path="/contact" component={Contact} />
+    </div>
+  )
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Fragment>
+        <TopMenu />
+        <Content />
+      </Fragment>
     );
   }
 }
