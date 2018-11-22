027ad5679b456a3a9e7a2b13f052d244aaa7788b
TOR <chayoud.p@arms-thai.com>
2018-11-22 16:24:58 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,59 +1,33 @@
 import React, { Component, Fragment } from 'react';
 import { Typography, Toolbar, Button } from '@material-ui/core';
-import { BrowserRouter as Link, Route } from 'react-router-dom';
 import './App.css';
 
 class TopMenu extends Component {
-  changeLink = locate => {
-    window.location.replace('/'+locate);
-  }
   render = () => {
     return (
       <Toolbar variant="dense">
         <Typography style={{flex:1}}>
-          <Button onClick={this.changeLink.bind(this,"")} variant="outlined">Home</Button>
+          <Button>Home</Button>
         </Typography>
-        <Button onClick={this.changeLink.bind(this,"experience")}>experience</Button>
-        <Button onClick={this.changeLink.bind(this,"skills")}>skills set</Button>
-        <Button onClick={this.changeLink.bind(this,"contact")}>contact</Button>
+        <Button>experience</Button>
+        <Button>skills set</Button>
+        <Button>contact</Button>
       </Toolbar>
     );
   }
 }
 
-const Home = () => {
-  return (
-    <p>Home</p>
-  )
-}
-
-const Experience = () => {
-  return (
-    <p>Experience</p>
-  )
-}
-
-const Skills = () => {
-  return (
-    <p>Skills</p>
-  )
-}
-
-const Contact = () => {
-  return (
-    <p>Contact</p>
-  )
-}
-
-const Content = () => {
-  return (
-    <div className="content">
-      <Route exact path="/" component={Home} />
-      <Route path="/experience" component={Experience} />
-      <Route path="/skills" component={Skills} />
-      <Route path="/contact" component={Contact} />
-    </div>
-  )
+class Content extends Component {
+  state = {
+    location: ''
+  }
+  render = () => {
+    return (
+      <div className="content">
+        <p>{this.state.location}</p>
+      </div>
+    )
+  }
 }
 
 class App extends Component {
